f487360b075ea6b6b3b0df5807978a83595c0f88
Omar Shaheen <omarshaheen2050@gmail.com>
2024-11-17 01:01:14 +0200

    edit auth btns

--- components/TopHeader/Buttons.vue
+++ components/TopHeader/Buttons.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="w-1/3 hidden lg:flex items-center justify-end gap-x-2">
+  <div class="w-1/3 hidden lg:flex items-center justify-end gap-x-4">
     <nuxt-link :to="localePath('/login')"
-      class="text-white bg-fp1 hover:bg-fp3 transition text-center font-bold rounded-lg text-md px-2 py-2 block focus:outline-none w-1/3"
+      class="custom_btn1 hover:animate-gradient-xy !text-sm !m-0 !p-1"
       v-if="!auth.isLoggedIn">
       <Icon name="lets-icons:sign-in-squre" class="text-2xl me-1" />
       {{ $t("login") }}
     </nuxt-link>
     <nuxt-link :to="localePath('/register')"
-      class="text-fp3 bg-transparent hover:bg-fp3 hover:text-white border transition text-center font-bold rounded-lg text-md px-2 py-1.5 block focus:outline-none w-1/3"
+      class="custom_btn1 hover:animate-gradient-xy !text-sm !m-0 !p-1"
       v-if="!auth.isLoggedIn">
       <Icon name="mdi-account-plus" class="text-2xl me-1" />
       {{ $t("create_new_account") }}
